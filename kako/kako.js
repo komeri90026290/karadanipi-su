d435010eb5b616287211f7320e9f34cd99fb8db2
komeri90026290 <kmm2359008@stu.o-hara.ac.jp>
2025-01-16 10:37:52 +0900

    homeに戻るボタン作成

--- kako/kako.js
+++ kako/kako.js
@@ -107,3 +107,9 @@ async function loadAndDisplayuserName(userId) {
     }
 }
 
+function modoru() {
+    const urlParams = new URLSearchParams(window.location.search);
+    const userId = Number(urlParams.get('userId')); // userIdを数値として取得
+    window.location.href = `../user/index.html?userId=${userId}`;
+  }
+
